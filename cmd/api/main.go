94540b1a71ab5fcc1518a4f6a82ebceb7eacd3bf
Yonathan1 <nathan.gunawan93@gmail.com>
2025-01-12 10:25:58 +0700

    Add Handlers & Chi Router

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -24,7 +24,7 @@ func main() {
 	log.Println("Starting application on port", port)
 
 	// start a web server
-	err := http.ListenAndServe(fmt.Sprintf(":%d", port), nil)
+	err := http.ListenAndServe(fmt.Sprintf(":%d", port), app.routes())
 	if err != nil {
 		log.Fatal(err)
 	}
